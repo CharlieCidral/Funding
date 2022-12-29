b2b08c8c1134a04c79ad93898e24b42f1466fc15
Charlie Cidral <charliesamoel@gmail.com>
2022-12-28 22:13:11 -0300

    first commit

---
+++ client/src/context/index.jsx
@@ -0,0 +1,52 @@
+import React, { useContext, createContext } from 'react';
+
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+    const { contract } = useContract('0x6014137f2205fBbC1E591a0bE2d149A9bAEf58F8')
+    const { mutateAsync: createCampaign } = 
+    useContractWrite(contract, 'createCampaign');
+
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+        try{
+            const data = await createCampaign([
+                address,
+                form.title,
+                form.description,
+                form.target,
+                new Date(form.deadline).getTime(),
+                form.image
+            ])
+            console.log("contract call success", data)
+        } catch (error) {
+            console.log("contract call failure", error)
+        }
+    }
+
+    const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns');
+        console.log(campaigns)
+    }
+
+    return (
+        <StateContext.Provider 
+            value={{
+                address,
+                contract,
+                connect,
+                createCampaign: publishCampaign,
+                getCampaigns,
+            }} 
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
